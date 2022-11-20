843e559c68409e670998acc72e1d8491c1a25d1d
Anne201 <118590269+Anne201@users.noreply.github.com>
2022-11-20 13:01:01 +0800

    v1

---
+++ multi01/match_menu_set.js
@@ -0,0 +1,15 @@
+menu_title='TPET 對對碰選單';
+menu_items = new Array(
+  '九九乘法-2,match_set_2x9.js'
+, '九九乘法-3,match_set_3x9.js'
+, '九九乘法-4,match_set_4x9.js'
+, '九九乘法-5,match_set_5x9.js'
+, '九九乘法-6,match_set_6x9.js'
+, '九九乘法-7,match_set_7x9.js'
+, '九九乘法-8,match_set_8x9.js'
+, '九九乘法-9,match_set_9x9.js'
+, '九九乘法-2-5,match_set_2_5x9.js'
+, '九九乘法-4-8,match_set_4_8x9.js'
+, '九九乘法-3-6,match_set_3_6x9.js'
+, '九九乘法-7-9,match_set_7_9x9.js'
+);
